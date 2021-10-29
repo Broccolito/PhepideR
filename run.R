da20da359f0efad3dc60d20fd5e5980f0d15e52c
Broccolito <wanjungu@yahoo.com>
2021-10-28 22:35:14 -0700

    edit run.R

--- run.R
+++ run.R
@@ -1,6 +1,35 @@
+get_directory = function(){
+  args <- commandArgs(trailingOnly = FALSE)
+  file <- "--file="
+  rstudio <- "RStudio"
+  match <- grep(rstudio, args)
+  if(length(match) > 0){
+    return(dirname(rstudioapi::getSourceEditorContext()$path))
+  }else{
+    match <- grep(file, args)
+    if (length(match) > 0) {
+      return(dirname(normalizePath(sub(file, "", args[match]))))
+    }else{
+      return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
+    }
+  }
+}
+
+setwd(get_directory())
+
 source("PhepideR.R")
 
 notch1 = PhepideR(chromosome = "9",
                   hg19_position = "139391338",
                   ref = "C",
                   alt = "T")
+
+fbn1 = PhepideR(chromosome = "15",
+                hg19_position = "48773926",
+                ref = "T",
+                alt = "C")
+
+eid1 = PhepideR(chromosome = "15",
+                hg19_position = "49237558",
+                ref = "C",
+                alt = "T")
